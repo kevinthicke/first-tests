d8ec456e7c023e42be367e858a74af1c93563509
Kevin Cabrera <keviincabrera@gmail.com>
2018-10-25 11:57:59 +0100

    first tests

--- function.js
+++ function.js
@@ -1,5 +1,7 @@
-function sum (num1, num2){
-    return num1 + num2;
+const functions = {
+    add: (num1, num2) => num1 + num2,
+    isNull: () => null,
+    checkValue: value => value
 }
 
-module.exports = sum;
+module.exports = functions;
--- function.test.js
+++ function.test.js
@@ -1,6 +1,19 @@
-let functionForTest = require("./function.js");
+let functionsForTest = require("./function.js");
 
-test("2+2 must be equal 4", ()=> {
-    let result = functionForTest(2,2);
-    expect(result).toBe(4);
+
+describe("my first tests", () => {
+    test("2+2 must be equal 4", ()=> {
+        let func = functionsForTest.add(2,2);
+        expect(func).toBe(4);
+    });
+
+    test("should be null", () => {
+        let func = functionsForTest.isNull();
+        expect(func).toBeNull(); 
+    });
+
+    test("should be falsy", () => {
+        let func = functionsForTest.checkValue(false);
+        expect(func).toBeFalsy();
+});
 });
